dba8c65295b266483b2a61d918e693c1ca334c8f
AvanesyanAshot <9teron9@mail.ru>
2020-09-06 19:04:00 +0300

    [test] query params

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express')
 
 const app = express()
 
+const product = ['brub', 'bruh']
+
 app.get('/', (req, res, next) => {
+    console.log(req.query.page)
     res.send('its working')
 })
-
+app.get('/products', (req, res, next) => {
+    console.log('Page', req.query.page)
+    res.send(product)
+})
+app.get('/products/:id', (req, res, next) => {
+    res.send(product[req.params.id])
+})
 app.listen(5000, () => {
-    console.log('Server started', new Date());
+    console.log('Server started', new Date())
 }) 
